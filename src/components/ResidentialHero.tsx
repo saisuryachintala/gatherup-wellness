069e62ff43da834164c35eefbbba8e2fcf8888ed
Sampath Kapurothu <S.Kapurothu@dickblick.com>
2025-12-19 00:29:48 +0530

    adjust solutions hero text and seperator lines scaling

--- src/components/ResidentialHero.tsx
+++ src/components/ResidentialHero.tsx
@@ -17,11 +17,11 @@ export const ResidentialHero: React.FC = () => {
 
             <div className="container mx-auto px-4 md:px-8 max-w-6xl relative z-10">
                 <div className="bg-[#053d3d] text-center w-[90%] mx-auto pt-10 pb-20">
-                    <p className="font-bold text-3xl md:text-4xl lg:text-6xl text-[#a6ff48]">
+                    <p className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-[#a6ff48] leading-tight">
                         Residential Properties<br />
                         & Multifamily Communities
                     </p>
-                    <div className="h-0.5 w-128 bg-[#a6ff48] mx-auto mt-10 -mb-20"></div>
+                    <div className="h-0.5 w-[60%] bg-[#a6ff48] mx-auto mt-10 -mb-20"></div>
                 </div>
                 
             </div>
